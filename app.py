e83e027e09e9b9f794a8ee61ae454f72799d0df1
SysToad64 <116862894+SysToad64@users.noreply.github.com>
2024-01-21 11:53:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from random import randint
+import random
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    if(randint()):
+    if(random.randint(0,1)):
         return 'Toads'
     else:
         return 'Frogs'
