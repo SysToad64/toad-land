b2a35cfcf3142f37bb62203b4441c5daae1e832d
SysToad64 <116862894+SysToad64@users.noreply.github.com>
2024-09-08 12:13:47 -0400

    Update app.py
    Add Minecraft Server Resource Pack Download

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
 import random
+import os
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/rand')
 def hello_world():
     if(random.randint(0,1)):
         return 'Toads'
     else:
         return 'Froads'
+@app.route('/serverpack_download')
+def server_pack():
+    with open("./server_pack.zip", "r") as f:
+        data = f.read(os.path.getsize("./server_pack.zip"))
+    return data
